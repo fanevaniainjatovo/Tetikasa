970d62a55386d717593d4776d9fd536ad14bc6bc
fanevaniainjatovo <654.faneva@gmail.com>
2023-11-20 18:46:03 +0300

    mise a jour

--- programmeC/Machine.h
+++
@@ -1,9 +0,0 @@
-
-typedef struct Machine{
-	char marque[30];
-	char mac[30];
-	char label[30];
-	
-	}Machine;
-
-void tri(Machine* label,int nbrM);	
--- programmeC/stdio.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-
-  printf("bdfg");
-  
-  return 0;
- }
